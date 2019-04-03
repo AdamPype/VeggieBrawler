8d8cfd955995e47d675f67f4ea7d9a508073eaf9
AdamPype <adam.pype@student.howest.be>
2019-04-03 17:00:03 +0200

    Added winscreen

--- Assets/Scripts/CameraScript.cs
+++ Assets/Scripts/CameraScript.cs
@@ -47,7 +47,8 @@ public class CameraScript : MonoBehaviour
             {
             foreach (Transform item in _objectsToTrack)
                 {
-                bounds.Encapsulate(item.position);
+                if (item)
+                    bounds.Encapsulate(item.position);
                 }
             }
         return bounds.center;
