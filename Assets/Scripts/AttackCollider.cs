e413dff7c099b08ec032e0b7975f990d74d1075a
LucasVanHooste <43540878+LucasVanHooste@users.noreply.github.com>
2019-04-08 14:53:54 +0200

    Added basic damage system (nog knockback or flinshing yet)

---
+++ Assets/Scripts/AttackCollider.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class AttackCollider : MonoBehaviour
+{
+    private PlayerScript _player;
+    public PlayerScript Opponent { get; private set; }
+
+    private void Start()
+    {
+        _player = transform.root.GetComponent<PlayerScript>(); //I know this is bad, shut up <3
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        PlayerScript hitPlayer = other.GetComponent<PlayerScript>();
+
+        if(hitPlayer!=null && hitPlayer != _player)
+        {
+            Opponent = hitPlayer;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        PlayerScript hitPlayer = other.GetComponent<PlayerScript>();
+
+        if (hitPlayer == Opponent)
+        {
+            Opponent = null;
+        }
+    }
+}
--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -8,8 +8,19 @@ using UnityEngine.Assertions;
 public class PlayerScript : MonoBehaviour
 {
     [SerializeField] [Range(1, 2)] private int _playerNumber=1;
-    [SerializeField] private float _attackCooldown;
-    [SerializeField] private float _specialAttackCooldown;
+
+    [Header("Attack fields")]
+    [SerializeField] private int _attackDamage=0;
+    [SerializeField] private float _attackCooldown=0;
+    [SerializeField] private AttackCollider[] _attackColliders;
+    [SerializeField] private Vector2 _attackDamageTimeRange=Vector2.zero;
+
+    [Header("Special attack fields")]
+    [SerializeField] private int _specialAttackDamage=0;
+    [SerializeField] private float _specialAttackCooldown=0;
+    [SerializeField] private AttackCollider[] _specialAttackColliders;
+    [SerializeField] private Vector2 _specialAttackDamageTimeRange=Vector2.zero;
+
 
     private Transform _transform;
     private PhysicsController _physicsController;
@@ -19,6 +30,7 @@ public class PlayerScript : MonoBehaviour
 
     private float _attackCooldownTimer;
     private float _specialAttackCooldownTimer;
+    private Coroutine _generalAttackCoroutine;
 
     void Start()
     {
@@ -40,31 +52,114 @@ public class PlayerScript : MonoBehaviour
             _physicsController.Jump = true;
         }
 
-        if(_attackCooldownTimer>_attackCooldown && _inputController.IsXButtonPressed(_playerNumber))
-        {
-            Attack();
-            _attackCooldownTimer = 0;
-        }
-        _attackCooldownTimer += Time.deltaTime;
-
-        if (_specialAttackCooldownTimer > _specialAttackCooldown && _inputController.IsYButtonPressed(_playerNumber))
-        {
-            SpecialAttack();
-            _specialAttackCooldownTimer = 0;
-        }
-        _specialAttackCooldownTimer += Time.deltaTime;
+        TryAttack();
+        TrySpecialAttack();
 
         _animationsController.Update();
     }
 
+    private void TryAttack()
+    {
+        if (_attackCooldownTimer > _attackCooldown && _inputController.IsXButtonPressed(_playerNumber))
+        {
+            Attack();
+        }
+        _attackCooldownTimer += Time.deltaTime;
+    }
+
+    private void TrySpecialAttack()
+    {
+        if (_specialAttackCooldownTimer > _specialAttackCooldown && _inputController.IsYButtonPressed(_playerNumber))
+        {
+            SpecialAttack();
+        }
+        _specialAttackCooldownTimer += Time.deltaTime;
+    }
+
     private void Attack()
     {
+        StartAttackCoroutine(TryAttackDamageOpponent());
         _animationsController.Attack();
+        _attackCooldownTimer = 0;
     }
 
     private void SpecialAttack()
     {
+        StartAttackCoroutine(TrySpecialAttackDamageOpponent());
         _animationsController.SpecialAttack();
+        _specialAttackCooldownTimer = 0;
+    }
+
+    private void StartAttackCoroutine(IEnumerator attack)
+    {
+        if (_generalAttackCoroutine != null)
+            StopCoroutine(_generalAttackCoroutine);
+
+        _generalAttackCoroutine = StartCoroutine(attack);
+    }
+
+    private IEnumerator TryAttackDamageOpponent()
+    {
+        Debug.Log("ATTACK");
+        float timer = 0;
+        while (timer < _attackDamageTimeRange.x)
+        {
+            timer += Time.deltaTime;
+            yield return null;
+        }
+
+        while (timer < _attackDamageTimeRange.y)
+        {
+            foreach (AttackCollider attackCollider in _attackColliders)
+            {
+                PlayerScript opponent = attackCollider.Opponent;
+                if (opponent != null)
+                {
+                    opponent.TakeDamage(_attackDamage);
+                    timer = _attackDamageTimeRange.y;
+                    break;
+                }
+            }
+
+            timer += Time.deltaTime;
+            yield return null;
+        }
+    }
+
+    private IEnumerator TrySpecialAttackDamageOpponent()
+    {
+        Debug.Log("SPECIAL ATTACK");
+        float timer = 0;
+        while (timer < _specialAttackDamageTimeRange.x)
+        {
+            timer += Time.deltaTime;
+            yield return null;
+        }
+
+        while (timer < _specialAttackDamageTimeRange.y)
+        {
+            foreach (AttackCollider specialAttackCollider in _specialAttackColliders)
+            {
+                PlayerScript opponent = specialAttackCollider.Opponent;
+                if (opponent != null)
+                {
+                    opponent.TakeDamage(_specialAttackDamage);
+                    timer = _specialAttackDamageTimeRange.y;
+                    break;
+                }
+            }
+
+            timer += Time.deltaTime;
+            yield return null;
+        }
+    }
+
+    public void TakeDamage(int damage)
+    {
+        if (_generalAttackCoroutine != null)
+            StopCoroutine(_generalAttackCoroutine);
+
+        Debug.Log("DAMAGE");
     }
 
     private void OnDrawGizmos()
